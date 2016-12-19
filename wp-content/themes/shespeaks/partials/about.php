65b0e0d32a5d03d8021ef76c2ca21891822690be
Galen Strasen <galen@wearetmbr.com>
2016-12-18 19:08:38 -0500

    About text

--- wp-content/themes/shespeaks/partials/about.php
+++ wp-content/themes/shespeaks/partials/about.php
@@ -7,11 +7,11 @@
         <h2 class="highlight"><span>Galen Strasen</span></h2>
         <p class="aqua">++++++</p>
         <p>I’m Galen Strasen, a Front-End Developer based in dreamy Oakland, California.</p>
-        <p>For the past 4 years I have had the pleasure of  working at TMBR - a rad little Creative Agency based in Jackson Hole, Wyoming. My focus has been in WordPress websites, where I am not only tasked with recreating beautiful web designs in code, but architecting custom content management systems tailored to the unique needs of each client. Want to know more? View the websites I consider to be my personal favorites, drop me a line, or check out the archives!  </p>
+        <p>For the past 4 years I have had the pleasure of  working at TMBR - a rad little <a href="http://wearetmbr.com" target="_blank">Creative Agency</a> based in Jackson Hole, Wyoming. My focus has been in WordPress websites, where I am not only tasked with recreating beautiful web designs in code, but architecting custom content management systems tailored to the unique needs of each client. Want to know more? View the websites I consider to be my personal favorites, <a href="tel:+19178165093">drop me a line</a>, or <a href="https://www.linkedin.com/in/galen-strasen-b5116112" target="_blank">connect with me on Linkedin!</a>  </p>
         <p><a href="mailto:gstrasen@gmail.com" class="btn">Get in Touch</a></p>
 
-        <p><a href="" class="underline">Call me / Beep me: 917.816.5093</a><br/>
-        <a href=""> Connect on LinkedIn</a></p>
+        <p class="hidden">Call me / Beep me: <a href="tel:+19178165093" class="underline">917.816.5093</a><br/>
+        Connect with me on <a href="https://www.linkedin.com/in/galen-strasen-b5116112" target="_blank" class="underline">LinkedIn</a></p>
         <img src="<?php bloginfo('template_directory');?>/public/images/headshot.jpg" alt="Galen Strasen" class="offset-img bottom-right" />
       </div>
     </div><!-- /col -->
