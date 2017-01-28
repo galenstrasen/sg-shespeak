c6282e81139062345df2d65e63d59a86f6deab0e
Galen Strasen <galen@wearetmbr.com>
2017-01-27 21:02:10 -0800

    BUILD

--- wp-content/themes/shespeaks/public/js/application.js
+++ wp-content/themes/shespeaks/public/js/application.js
@@ -592,15 +592,17 @@ var Throttle = function($) { // ----- static module
     var _init = function() {
         // Window Scroll functions
         $(window).on('scroll', _throttle(function(){
-
+var body = $('body.home');
+// if(body.hasClass('scrollah')){ body.removeClass('scrollah'); }
+// else { body.addClass('scrollah'); }
            var  scrollTop = $(window).scrollTop(),
                 screensize = $('.js-doc-nav-wrapper').height() - 50,
                 distance = (screensize - scrollTop);
 
-            if ( distance < 0 ) {
-                $('.navbar-default').addClass("js-nav-scroll-white");
+            if ( distance < 0 ||   $(window).scrollTop() > 0 ) {
+                body.addClass('scrollah');
             } else {
-                $('.navbar-default').removeClass("js-nav-scroll-white");
+                body.removeClass('scrollah');
             }
 
         }, throttleTimeOut));
@@ -621,6 +623,53 @@ var Throttle = function($) { // ----- static module
         init: _init
     };
 }(jQuery);
+
+// Credit goes to [Underscore.js](http://underscorejs.org/)
+
+/**
+ * Returns a function, that, when invoked, will only be triggered at most
+ * once during a given window of time. Normally, the throttled function will
+ * run as much as it can, without ever going more than once per wait
+ * duration; but if you’d like to disable the execution on the leading edge,
+ * pass {leading: false}. To disable execution on the trailing edge, ditto.
+ */
+
+// throttle's dependent upon _now
+_now = Date.now || function() {
+  return new Date().getTime();
+};
+
+_throttle = function(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function() {
+    previous = options.leading === false ? 0 : _now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function() {
+    var now = _now();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+};
 
 // TMBR Creative Agency
 // Author: michael.ross
@@ -1317,7 +1366,7 @@ var Control = function($) { // ----- static module
     var _init = function() {
 
         Preloader.init();
-        //Throttle.init();
+        Throttle.init();
         SmoothScroll.init();
         Animated.init();
         StickyNav.init();
